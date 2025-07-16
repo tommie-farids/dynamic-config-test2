a62fbc052ca54ff8788535d9515a99890ddbec97
Danny Santino <dev@dannysantino.com>
2025-07-16 15:08:46 +0100

    fix(ci): resolve db setup issue

---
+++ server/db/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS carastoremini;
+
+USE carastoremini;
+
+GRANT ALL ON carastoremini.* TO 'admin'@'localhost';
+
+FLUSH PRIVILEGES;
+
+DROP TABLE IF EXISTS;
+
+CREATE TABLE products(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    img VARCHAR(255) NOT NULL
+);
